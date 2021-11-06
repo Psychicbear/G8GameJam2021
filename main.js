49ffa4e11f534544e08e83f56f8faf509abe966b
Riley <rjscho@live.com.au>
2021-11-06 12:22:40 +1000

    added player class and changed main.js to test

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 //this is the one you'll write your code in make sure to change the script tag in index.html to use main.js instead of test.js
-let runframes = [
-    {'name':'run1', 'frame':{'x':408, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'run2', 'frame':{'x':432, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'run3', 'frame':{'x':456, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'run4', 'frame':{'x':480, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'run5', 'frame':{'x':504, 'y': 0, 'width': 24, 'height': 24}}
-]
-
-let walkframes = [
-    {'name':'walk1', 'frame':{'x':72, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'walk2', 'frame':{'x':96, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'walk3', 'frame':{'x':120, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'walk4', 'frame':{'x':144, 'y': 0, 'width': 24, 'height': 24}},
-    {'name':'walk5', 'frame':{'x':168, 'y': 0, 'width': 24, 'height': 24}}
-]
 let enemies, expPoints;
 function preload(){
     redXP = loadImage('redXP.png');
     loadSprites = loadImage('doux.png')
-    walkAnimation = animationFromSpriteSheet(loadSprites, walkframes)
-    runAnimation = animationFromSpriteSheet(loadSprites, runframes)
+    animJSON = loadJSON('playerAnimations.json', (data) =>{
+        walkAnimation = animationFromSpriteSheet(loadSprites, animJSON['walk'])
+        runAnimation = animationFromSpriteSheet(loadSprites, animJSON['run'])
+    })
 }
+
 
 function setup(){
     createCanvas(1280, 720)
+    frameRate(60)
     enemies = new Group();
     expPoints = new Group();
+    terrainGroup = new Group();
+    player = new Player(320,320, 24, 24)
+    player.s.addAnimation('walk', walkAnimation)
+    player.s.setCollider('circle', 0,0,6)
+    player.s.scale = 4
+    player.s.debug = true
+    floorSprite = createSprite(640,700,1280,20)
     enemy = new Enemy('red', 200, 200, 30, 30);
-    dino = createSprite(320,320, 24, 24)
-    dino.addAnimation('run', runAnimation)
-    dino.addAnimation('walk', walkAnimation)
-    dino.setCollider('rectangle',0,0,24,24)
-    dino.debug = true
-    dino.scale = 4
+    //dino = createSprite(320,320, 24, 24)
+    //dino.setCollider('rectangle',0,0,24,24)
+    //dino.debug = true
+    //dino.scale = 4
 }
 
 function draw(){
+    frameRate(60)
+    framerate = getFrameRate()
+    console.log(framerate)
+    color(0,255,100)
+    textSize(20)
+    text('FPS:' + framerate, 50,50)
     background(0);
+    player.s.collide(floorSprite, ()=>{
+        player.airborne = 0    
+    })
+    player.keyInputs()
     enemy.collisionCheck();
 
-    animationHandling()
+    //animationHandling()
     drawSprites();
 }
 
@@ -56,6 +60,16 @@ function animationHandling(){
 
 //Takes a spritesheet image, and an array of frames, and produces as animation which can be attached to sprites
 function animationFromSpriteSheet(img, frames){
+    console.log(frames)
     spriteSheet = loadSpriteSheet(img, frames)
-    return loadAnimation(spriteSheet)
+    animation = loadAnimation(spriteSheet)
+    return animation
+}
+
+function jumptest(){
+    if(keyIsDown(32)){
+        if(keyWentUp(32)){
+            console.log("the key was released")
+        }
+    }
 }
---
+++ player.js
@@ -0,0 +1,51 @@
+class Player{
+    constructor(x,y,w,h){
+        this.s = createSprite(x,y,w,h)
+        this.s.maxSpeed = 10
+        this.maxHP = 12
+        this.curHP = 12
+        this.airborne = 0
+        this.airTime = 30
+        this.attackDmg = 1
+        this.attackSpd = 70
+        this.attack = 0
+        this.hurtTime = 90
+        this.hurt = 0
+    }
+
+    keyInputs(){
+        //Move left or right
+        if(keyIsDown(RIGHT_ARROW) && keyIsDown(LEFT_ARROW)){
+            this.s.addSpeed(0,0)
+        } else if(keyIsDown(RIGHT_ARROW)){
+            console.log('Move Right')
+            this.s.addSpeed(10,0)
+        } else if(keyIsDown(LEFT_ARROW)){
+            this.s.addSpeed(10,180)
+        } else {
+            this.s.velocity.lerp(createVector(0 , this.s.velocity.y), 0.1)
+        }
+
+        if(keyIsDown(32) && this.airborne < this.airTime){//If jump button held and this.airborne timer less than this.airTime
+            this.jump()
+        } else if(keyWentUp(32) && this.airborne){//If jump button released and this.airborne == true (not 0)
+            this.airborne = this.airTime//Disallow player to press jump again until this.airborne set to 0
+            this.s.addSpeed(10, 90)
+        } else{this.s.addSpeed(10,90)}
+    }
+
+    attack(){
+        //placeholder
+    }
+
+    move(direction){
+        //placeholder
+        variable = direction
+    }
+
+    jump(){
+        this.s.addSpeed(30,-90)
+        this.airborne++
+    }
+
+}
